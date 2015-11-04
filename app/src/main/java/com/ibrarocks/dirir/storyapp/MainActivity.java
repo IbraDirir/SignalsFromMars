cda1d93c67222ceafe45e63ae51b220b6cc6817e
ibra-dirir <khaliildirir@gmail.com>
2015-11-04 06:18:35 -0800

    created new story activity

--- app/src/main/java/com/ibrarocks/dirir/storyapp/MainActivity.java
+++ app/src/main/java/com/ibrarocks/dirir/storyapp/MainActivity.java
@@ -1,21 +1,29 @@
 package com.ibrarocks.dirir.storyapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mNameField;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    mNameField = (EditText)findViewById(R.id.NameEditText);
+    mStartButton = (Button) findViewById(R.id.StartButton);
+    mStartButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        String name = mNameField.getText().toString();
+            Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+        }
+    });
 
     }
 
